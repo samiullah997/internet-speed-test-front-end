f56f8a7f1c80992e853b9debf5a934b847aebdeb
samiullah997 <samiullahk997@gmail.com>
2023-04-17 14:51:36 +0500

    add div navebar file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ const Navbar = () => (
     <div>
       <h2 className="text-4xl text-gray-600 font-semibold">Places</h2>
     </div>
-    
+    <div className="flex items-center justify-between">
+      
+    </div>
   </div>
 );
 
