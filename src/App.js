17cf999c5f19788db7c5b3d0254cad46ce844a36
samiullah997 <samiullahk997@gmail.com>
2023-05-09 15:03:00 +0500

    update app.js file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/redux/config/store';
 import PlaceList from './components/PlaceList';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>
+      <div className="bg-white p-8 rounded-md w-full">
+        <Navbar />
         <Routes>
           <Route index element={<PlaceList />} />
         </Routes>
