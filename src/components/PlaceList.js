b518af146905467e021e872a68fb3f692800737c
samiullah997 <samiullahk997@gmail.com>
2023-05-19 15:06:16 +0500

    complete list styling

--- src/components/PlaceList.js
+++ src/components/PlaceList.js
@@ -58,7 +58,9 @@ const PlaceList = () => {
                 <tr key={place.name}>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <div className="flex items-center">
-                      
+                      <div className="flex-shrink-0 w-10 h-10">
+                        <img className="w-full h-full rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" alt="profile" />
+                      </div>
                       <div className="ml-3">
                         <p className="text-gray-900 whitespace-no-wrap">
                           {place.name}
