5cf0ff6caa9e91a63b3eed29b7f298b7012f3023
samiullah997 <samiullahk997@gmail.com>
2023-05-20 14:49:44 +0500

    create navebar file

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div className="flex items-center justify-between pb-6">
+    <div>
+      <h2 className="text-4xl text-gray-600 font-semibold">Places</h2>
+    </div>
+    
+  </div>
+);
+
+export default Navbar;
