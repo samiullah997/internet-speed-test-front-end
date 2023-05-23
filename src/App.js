7bc9c4d584072205638d4ed8f234833246424ce0
samiullah997 <samiullahk997@gmail.com>
2023-05-23 13:07:20 +0500

    Update App js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/redux/config/store';
 import PlaceList from './components/PlaceList';
-import InternetSpeeds from './components/InternetSpeeds';
 
 const App = () => (
   <Provider store={store}>
@@ -11,7 +10,6 @@ const App = () => (
       <div className="bg-white p-8 rounded-md w-full">
         <Routes>
           <Route index element={<PlaceList />} />
-          <Route path="new-internet-speed" element={<InternetSpeeds />} />
         </Routes>
       </div>
     </Router>
